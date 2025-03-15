6ad6656ae702feabdd77fd4d533efc589c45cd47
Caiden Henn <caidensky@gmail.com>
2025-03-15 16:33:51 -0400

    Made the tab stuff into seperate classes for clarity

--- dashboard.py
+++ dashboard.py
@@ -13,6 +13,7 @@ class ServerDisplay(Thread):
 		self.cpu=0
 		self.memory = 0
 		self.disk = 0
+		self.IO =0
 
 	def run(self):
 		"""Run the thread"""
@@ -23,12 +24,46 @@ class ServerDisplay(Thread):
 	def fetch_stats(self):
 		"""Fetch server stats from API"""
 		response = requests.get(self.webpage_link)
+		
 		data = response.json()
+		print(data)
 		self.cpu = data["CPU"]
 		self.memory = data["Memory"]
 		self.disk = data["HDD (Disk)"]
+		self.IO
 		
-		
+class TabClass:
+	def __init__(self,thread):
+		self.server_display=thread
+		self.error_placeholder=st.empty()
+		self.cpu_placeholder = st.empty()
+		self.memory_placeholder = st.empty()
+		self.disk_placeholder = st.empty()
+		self.line_chart_placeholder=st.empty()
+		self.cpu_data=[]
+		self.memory_data=[]
+		self.disk_data=[]
+		self.timestamps=[]
+	def update_tab(self):
+		if (self.server_display.cpu,self.server_display.memory,self.server_display.disk)==(0,0,0):
+			self.error_placeholder.subheader("No connection")
+			return
+		timestamp = time.strftime('%H:%M:%S')
+		self.cpu_data.append(self.server_display.cpu)
+		self.memory_data.append(self.server_display.memory)
+		self.disk_data.append(self.server_display.disk)
+		self.timestamps.append(timestamp)
+		data = {
+			"Timestamp": self.timestamps,
+			"CPU (%)": self.cpu_data,
+			"Memory (%)": self.memory_data,
+			"Disk (%)": self.disk_data,
+		}
+		stats_df = pd.DataFrame(data)
+		self.line_chart_placeholder.line_chart(stats_df,x='Timestamp',y=['CPU (%)',"Memory (%)","Disk (%)"])
+		self.cpu_placeholder.metric("CPU Usage", f"{self.server_display.cpu}%")
+		self.memory_placeholder.metric("Memory Usage", f"{self.server_display.memory}%")
+		self.disk_placeholder.metric("Disk Usage", f"{self.server_display.disk}%")
 	
 def main():
 	st.title("Linux Monitoring Dashboard")
@@ -36,85 +71,28 @@ def main():
 	#make threads to fetch api data from different servers
 	server_display1=ServerDisplay("http://35.231.107.155:5000/stats")
 	server_display1.start()
-	server_display2=ServerDisplay("ip_here")
+	server_display2=ServerDisplay("http://34.133.20.113:5000/stats")
 	server_display2.start()
-	server_display2=ServerDisplay("ip_here")
-	server_display2.start()
-	cpu_data1=[]
-	memory_data1=[]
-	disk_data1=[]
-	timestamps1=[]
+	server_display3=ServerDisplay("ip_here")
+	server_display3.start()
+	
 
 	cpu_data2=[]
 	memory_data2=[]
 	disk_data2=[]
 	timestamps2=[]
 	with tab1:
-		error_placeholder=st.empty()
-		cpu_placeholder1 = st.empty()
-		memory_placeholder1 = st.empty()
-		disk_placeholder1 = st.empty()
-		line_chart_placeholder1=st.empty()
-		def update_tab1():
-			if (server_display1.cpu,server_display1.memory,server_display1.disk)==(0,0,0):
-				print('YUP')
-				error_placeholder.subheader("No connection")
-				return
-			timestamp = time.strftime('%H:%M:%S')
-			cpu_data1.append(server_display1.cpu)
-			memory_data1.append(server_display1.memory)
-			disk_data1.append(server_display1.disk)
-			timestamps1.append(timestamp)
-			data = {
-                "Timestamp": timestamps1,
-                "CPU (%)": cpu_data1,
-                "Memory (%)": memory_data1,
-                "Disk (%)": disk_data1,
-            }
-			stats_df = pd.DataFrame(data)
-			line_chart_placeholder1.line_chart(stats_df,x='Timestamp',y=['CPU (%)',"Memory (%)","Disk (%)"])
-			cpu_placeholder1.metric("CPU Usage", f"{server_display1.cpu}%")
-			memory_placeholder1.metric("Memory Usage", f"{server_display1.memory}%")
-			disk_placeholder1.metric("Disk Usage", f"{server_display1.disk}%")
-			
-		
+		tab1_update_class=TabClass(server_display1)
 	with tab2:
-		error_placeholder=st.empty()
-		cpu_placeholder2 = st.empty()
-		memory_placeholder2 = st.empty()
-		disk_placeholder2 = st.empty()
-		line_chart_placeholder2=st.empty()
-		def update_tab2():
-			if (server_display2.cpu,server_display2.memory,server_display2.disk)==(0,0,0):
-				print('YUP')
-				error_placeholder.subheader("No connection")
-				return
-			error_placeholder.empty()
-			timestamp = time.strftime('%H:%M:%S')
-			cpu_data2.append(server_display2.cpu)
-			memory_data2.append(server_display2.memory)
-			disk_data2.append(server_display2.disk)
-			timestamps2.append(timestamp)
-			data = {
-                "Timestamp": timestamps2,
-                "CPU (%)": cpu_data2,
-                "Memory (%)": memory_data2,
-                "Disk (%)": disk_data2,
-            }
-			stats_df = pd.DataFrame(data)
-			line_chart_placeholder2.line_chart(stats_df,x='Timestamp',y=['CPU (%)',"Memory (%)","Disk (%)"])
-			cpu_placeholder2.metric("CPU Usage", f"{server_display2.cpu}%")
-			memory_placeholder2.metric("Memory Usage", f"{server_display2.memory}%")
-			disk_placeholder2.metric("Disk Usage", f"{server_display2.disk}%")
+		tab2_update_class=TabClass(server_display2)
 	with tab3:
-		#st.header("An owl")
-		st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+		tab3_update_class=TabClass(server_display3)
 	
 	while True:
 		#this is the logic loop that calls updates
-		update_tab1()
-		update_tab2()
-		time.sleep(5)
+		tab1_update_class.update_tab()
+		tab2_update_class.update_tab()
+		time.sleep(2)
 
 if __name__ == '__main__':
 	main()
