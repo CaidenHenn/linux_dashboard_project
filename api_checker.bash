b22a7eafb0a17049186ebfe067ad01a624bf3384
Natasha <NatTheCat1972@yahoo.com>
2025-03-16 20:25:39 -0400

    Commented api_check.bash
    Provided comments to explain what is going on in api_check.bash

--- api_checker.bash
+++ api_checker.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
+
+#check if pi_script.py is running
 if ps aux | grep '[a]pi_script.py' >/dev/null; then
-    exit
+    exit #exit if running
 else
     set -e
-    source "./venv/bin/activate"
-    python3 sys_stats.py &
+    source "./venv/bin/activate" #enter the vitural environment
+    python3 sys_stats.py &  #run the API script in the background
 fi
